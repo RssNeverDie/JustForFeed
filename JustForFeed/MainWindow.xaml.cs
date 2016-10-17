df322d6e2967fc4e340dc100fc679d74350e26d0
Jess Smith <shisujieit@gmail.com>
2016-10-17 18:10:21 +0800

    SyndicationFeed-.net原生rss解析类测试；

--- JustForFeed/MainWindow.xaml.cs
+++ JustForFeed/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace JustForFeed
 {
@@ -23,6 +26,24 @@ namespace JustForFeed
         public MainWindow()
         {
             InitializeComponent();
+
+            ////XmlReader r = XmlReader.Create("http://www.shisujie.com/rss?containerid=31");
+            //SyndicationFeed test = SyndicationFeed.Load(r); // new SyndicationFeed("", "", new Uri("http://www.shisujie.com/rss?containerid=31"));
+
+            //IEnumerable<SyndicationItem> aa = test.Items;
+            HtmlAgilityPack.HtmlDocument a = new HtmlAgilityPack.HtmlDocument();
+            a.LoadHtml("<p>aaaa</p>");
+            WebBrowser web = new WebBrowser();
+            web.NavigateToString("<p>aaaa</p><img title=\"changerowgroupproperty\"  alt=\"修改行组属性\" src=\"http://www.canself.com/Media/Default/Open-Live-Writer/ReportViewer_1372A/image_thumb_2.png\">");
+            this.frame.Navigate(web);
+          
+
         }
+        
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        
     }
 }
